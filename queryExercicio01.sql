bdd8373da10b2dbd73c57a016256e3e0bbced218
Otavio de Mello <otaviodemello15@gmail.com>
2022-03-05 19:27:02 -0300

    Exercicio 01

---
+++ queryExercicio01.sql
@@ -0,0 +1,11 @@
+create database db_exercicio01;
+use db_exercicio01;
+
+create table product 
+(produc_id int auto_increment primary key, 
+product_name varchar(50) not null, 
+product_description varchar(150) not null, 
+product_amount int not null,
+product_price decimal(9, 2) unsigned not null);
+
+
