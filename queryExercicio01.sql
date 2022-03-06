57918963546c3fc41eab7fc4b9910632d363f703
Otavio de Mello <otaviodemello15@gmail.com>
2022-03-06 08:23:27 -0300

    Correção nome coluna

--- queryExercicio01.sql
+++ queryExercicio01.sql
@@ -2,7 +2,7 @@ create database db_exercicio01;
 use db_exercicio01;
 
 create table product 
-(produc_id int auto_increment primary key, 
+(product_id int auto_increment primary key, 
 product_name varchar(50) not null, 
 product_description varchar(150) not null, 
 product_amount int not null,
