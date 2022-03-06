c742aca876e0cc85d60bc0aef58cc6b7a4aed4f3
Otavio de Mello <otaviodemello15@gmail.com>
2022-03-06 12:43:14 -0300

    Correção queryExercicio02

--- queryExercicio02.sql
+++ queryExercicio02.sql
@@ -1,1 +1,9 @@
 create database db_exercicio02;
+use db_exercicio02;
+
+create table movie(
+movie_id int auto_increment primary key,
+movie_name varchar(50) not null,
+movie_description varchar(200) not null,
+movie_year year(4) not null
+);
